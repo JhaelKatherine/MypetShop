da8bce7a3b13d574e369391371303f0c7f08d3eb
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-14 21:31:36 -0400

    -112

--- frontend/src/screens/ForgetPasswordScreen.js
+++ frontend/src/screens/ForgetPasswordScreen.js
@@ -1,58 +1,63 @@
-import Axios from 'axios';
-import { useContext, useEffect, useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import { Helmet } from 'react-helmet-async';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { Store } from '../Store';
-import { getError } from '../utils';
+// Importación de módulos y componentes necesarios
+import Axios from 'axios'; // Importa el cliente Axios para hacer solicitudes HTTP
+import { useContext, useEffect, useState } from 'react'; // Importa hooks de React para manejar estados y efectos
+import Container from 'react-bootstrap/Container'; // Importa el contenedor de Bootstrap de React
+import Button from 'react-bootstrap/Button'; // Importa el botón de Bootstrap de React
+import Form from 'react-bootstrap/Form'; // Importa el formulario de Bootstrap de React
+import { Helmet } from 'react-helmet-async'; // Importa Helmet para gestionar el título de la página dinámicamente
+import { useNavigate } from 'react-router-dom'; // Importa hook para navegación en React Router
+import { toast } from 'react-toastify'; // Importa la librería para notificaciones de toast
+import { Store } from '../Store'; // Importa el contexto Store
+import { getError } from '../utils'; // Importa una función utilitaria para obtener errores
 
+// Componente ForgetPasswordScreen
 export default function ForgetPasswordScreen() {
-  const navigate = useNavigate();
+  const navigate = useNavigate(); // Obtiene la función de navegación
 
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState(''); // Estado local para almacenar el email del usuario
 
-  const { state } = useContext(Store);
-  const { userInfo } = state;
+  const { state } = useContext(Store); // Obtiene el estado global del contexto
+  const { userInfo } = state; // Extrae la información del usuario del estado global
 
+  // Efecto que redirige al usuario si ya ha iniciado sesión
   useEffect(() => {
     if (userInfo) {
-      navigate('/');
+      navigate('/'); // Redirige a la página principal si el usuario ya está autenticado
     }
   }, [navigate, userInfo]);
 
+  // Función que maneja el envío del formulario
   const submitHandler = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Previene el comportamiento por defecto del formulario
     try {
       const { data } = await Axios.post('/api/users/forget-password', {
-        email,
+        email, // Envía una solicitud POST con el email proporcionado por el usuario
       });
-      toast.success(data.message);
+      toast.success(data.message); // Muestra un mensaje de éxito si la solicitud es exitosa
     } catch (err) {
-      toast.error(getError(err));
+      toast.error(getError(err)); // Muestra un mensaje de error si la solicitud falla
     }
   };
 
   return (
+    // Componente de contenedor
     <Container className="small-container">
       <Helmet>
-        <title>Forget Password</title>
+        <title>Forget Password</title> // Establece el título de la página dinámicamente
       </Helmet>
-      <h1 className="my-3">Forget Password</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email</Form.Label>
+      <h1 className="my-3">Forget Password</h1> // Título de la página
+      <Form onSubmit={submitHandler}> // Formulario que llama a la función submitHandler al enviar
+        <Form.Group className="mb-3" controlId="email"> // Grupo de formulario para el email
+          <Form.Label>Email</Form.Label> // Etiqueta para el campo de email
           <Form.Control
-            type="email"
-            required
-            onChange={(e) => setEmail(e.target.value)}
+            type="email" // Campo de entrada de tipo email
+            required // Campo obligatorio
+            onChange={(e) => setEmail(e.target.value)} // Actualiza el estado del email al cambiar el valor del campo
           />
         </Form.Group>
 
         <div className="mb-3">
-          <Button type="submit">submit</Button>
+          <Button type="submit">submit</Button> // Botón para enviar el formulario
         </div>
       </Form>
     </Container>
--- frontend/src/screens/MapScreen.js
+++ frontend/src/screens/MapScreen.js
@@ -11,24 +11,31 @@ import { Store } from '../Store';
 import Button from 'react-bootstrap/Button';
 import { toast } from 'react-toastify';
 
+// Ubicación predeterminada para el mapa
 const defaultLocation = { lat: 45.516, lng: -73.56 };
+
+// Bibliotecas requeridas para el mapa
 const libs = ['places'];
 
 export default function MapScreen() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
   const navigate = useNavigate();
+
+  // Estados para manejar la API de Google Maps y la ubicación
   const [googleApiKey, setGoogleApiKey] = useState('');
   const [center, setCenter] = useState(defaultLocation);
   const [location, setLocation] = useState(center);
 
+  // Referencias para el mapa, el cuadro de búsqueda y el marcador
   const mapRef = useRef(null);
   const placeRef = useRef(null);
   const markerRef = useRef(null);
 
+  // Función para obtener la ubicación actual del usuario
   const getUserCurrentLocation = () => {
     if (!navigator.geolocation) {
-      alert('Geolocation os not supported by this browser');
+      alert('Geolocation is not supported by this browser');
     } else {
       navigator.geolocation.getCurrentPosition((position) => {
         setCenter({
@@ -42,24 +49,34 @@ export default function MapScreen() {
       });
     }
   };
+
+  // Efecto para obtener la clave de la API de Google Maps y la ubicación actual al cargar el componente
   useEffect(() => {
-    const fetch = async () => {
-      const { data } = await axios('/api/keys/google', {
-        headers: { Authorization: `BEARER ${userInfo.token}` },
-      });
-      setGoogleApiKey(data.key);
-      getUserCurrentLocation();
+    const fetchData = async () => {
+      try {
+        // Llamada para obtener la clave de la API de Google Maps
+        const { data } = await axios('/api/keys/google', {
+          headers: { Authorization: `BEARER ${userInfo.token}` },
+        });
+        setGoogleApiKey(data.key);
+        getUserCurrentLocation();
+      } catch (error) {
+        // Manejo de errores en caso de fallo en la obtención de la clave de la API
+        console.error('Error fetching Google API key:', error);
+      }
     };
 
-    fetch();
+    fetchData(); // Llamada a la función para obtener la clave de la API y la ubicación actual
     ctxDispatch({
       type: 'SET_FULLBOX_ON',
     });
-  }, [ctxDispatch]);
+  }, [ctxDispatch, userInfo.token]);
 
+  // Callbacks para eventos de carga y cambios en el mapa y el cuadro de búsqueda
   const onLoad = (map) => {
     mapRef.current = map;
   };
+
   const onIdle = () => {
     setLocation({
       lat: mapRef.current.center.lat(),
@@ -70,6 +87,7 @@ export default function MapScreen() {
   const onLoadPlaces = (place) => {
     placeRef.current = place;
   };
+
   const onPlacesChanged = () => {
     const place = placeRef.current.getPlaces()[0].geometry.location;
     setCenter({ lat: place.lat(), lng: place.lng() });
@@ -80,6 +98,7 @@ export default function MapScreen() {
     markerRef.current = marker;
   };
 
+  // Función para confirmar la selección de ubicación y guardarla en el estado global
   const onConfirm = () => {
     const places = placeRef.current.getPlaces() || [{}];
     ctxDispatch({
@@ -93,12 +112,14 @@ export default function MapScreen() {
         googleAddressId: places[0].id,
       },
     });
-    toast.success('location selected successfully.');
+    toast.success('Location selected successfully.');
     navigate('/shipping');
   };
+
   return (
     <div className="full-box">
       <LoadScript libraries={libs} googleMapsApiKey={googleApiKey}>
+        {/* Componente GoogleMap proporciona el mapa */}
         <GoogleMap
           id="smaple-map"
           mapContainerStyle={{ height: '100%', width: '100%' }}
@@ -107,10 +128,12 @@ export default function MapScreen() {
           onLoad={onLoad}
           onIdle={onIdle}
         >
+          {/* Componente StandaloneSearchBox proporciona el cuadro de búsqueda */}
           <StandaloneSearchBox
             onLoad={onLoadPlaces}
             onPlacesChanged={onPlacesChanged}
           >
+            {/* Entrada para la búsqueda y botón para confirmar la ubicación */}
             <div className="map-input-box">
               <input type="text" placeholder="Enter your address"></input>
               <Button type="button" onClick={onConfirm}>
@@ -118,6 +141,7 @@ export default function MapScreen() {
               </Button>
             </div>
           </StandaloneSearchBox>
+          {/* Componente Marker muestra un marcador en la ubicación seleccionada */}
           <Marker position={location} onLoad={onMarkerLoad}></Marker>
         </GoogleMap>
       </LoadScript>
