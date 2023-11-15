629f5a53332415d33dc73e47330520a4cf1c70da
JhaelArce <jhael.arce.8041022@usalesiana.edu.bo>
2023-11-14 21:03:53 -0400

    --

--- frontend/src/screens/OrderHistoryScreen.js
+++ frontend/src/screens/OrderHistoryScreen.js
@@ -1,13 +1,15 @@
-import React, { useContext, useEffect, useReducer } from 'react';
-import { Helmet } from 'react-helmet-async';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
-import { Store } from '../Store';
-import { getError } from '../utils';
-import Button from 'react-bootstrap/esm/Button';
+// Importaciones de módulos y componentes necesarios
+import React, { useContext, useEffect, useReducer } from 'react'; // Importación de elementos de React
+import { Helmet } from 'react-helmet-async'; // Para manipular los elementos <head> del HTML de manera asíncrona
+import axios from 'axios'; // Para hacer peticiones HTTP
+import { useNavigate } from 'react-router-dom'; // Para la navegación
+import LoadingBox from '../components/LoadingBox'; // Componente para mostrar cuando se está cargando información
+import MessageBox from '../components/MessageBox'; // Componente para mostrar mensajes de error o éxito
+import { Store } from '../Store'; // Contexto global de la aplicación
+import { getError } from '../utils'; // Función utilitaria para obtener mensajes de error
+import Button from 'react-bootstrap/esm/Button'; // Botón de Bootstrap
 
+// Reducer para manejar el estado de carga, errores y pedidos
 const reducer = (state, action) => {
   switch (action.type) {
     case 'FETCH_REQUEST':
@@ -21,46 +23,51 @@ const reducer = (state, action) => {
   }
 };
 
+// Componente principal para mostrar el historial de pedidos
 export default function OrderHistoryScreen() {
-  const { state } = useContext(Store);
-  const { userInfo } = state;
-  const navigate = useNavigate();
+  const { state } = useContext(Store); // Acceso al contexto global de la aplicación
+  const { userInfo } = state; // Información del usuario logueado
+  const navigate = useNavigate(); // Función para navegar entre páginas
 
+  // Estado y dispatch del reducer para manejar los pedidos
   const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
     loading: true,
     error: '',
   });
+
+  // Efecto para cargar los pedidos del usuario
   useEffect(() => {
     const fetchData = async () => {
-      dispatch({ type: 'FETCH_REQUEST' });
+      dispatch({ type: 'FETCH_REQUEST' }); // Indica que se está realizando una petición
       try {
-        const { data } = await axios.get(
-          `/api/orders/mine`,
-
-          { headers: { Authorization: `Bearer ${userInfo.token}` } }
-        );
-        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+        // Petición GET para obtener los pedidos del usuario
+        const { data } = await axios.get(`/api/orders/mine`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` }, // Se incluye el token de autenticación
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data }); // Se guardan los pedidos obtenidos
       } catch (error) {
-        dispatch({
+        dispatch({ // Si hay un error en la petición, se maneja y se muestra al usuario
           type: 'FETCH_FAIL',
           payload: getError(error),
         });
       }
     };
-    fetchData();
-  }, [userInfo]);
+    fetchData(); // Ejecuta la función para obtener los datos al cargar el componente
+  }, [userInfo]); // Se ejecuta cada vez que userInfo cambia
+
   return (
     <div>
       <Helmet>
-        <title>Order History</title>
+        <title>Order History</title> {/* Se modifica el título de la página */}
       </Helmet>
 
-      <h1>Order History</h1>
-      {loading ? (
+      <h1>Order History</h1> {/* Encabezado de la página */}
+      {loading ? ( // Si está cargando, se muestra el componente LoadingBox
         <LoadingBox></LoadingBox>
-      ) : error ? (
+      ) : error ? ( // Si hay un error, se muestra el componente MessageBox
         <MessageBox variant="danger">{error}</MessageBox>
       ) : (
+        // Si no hay errores ni está cargando, se muestran los pedidos en una tabla
         <table className="table">
           <thead>
             <tr>
@@ -73,6 +80,7 @@ export default function OrderHistoryScreen() {
             </tr>
           </thead>
           <tbody>
+            {/* Mapeo de los pedidos para mostrarlos en la tabla */}
             {orders.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
@@ -85,6 +93,7 @@ export default function OrderHistoryScreen() {
                     : 'No'}
                 </td>
                 <td>
+                  {/* Botón para ver los detalles del pedido */}
                   <Button
                     type="button"
                     variant="light"
